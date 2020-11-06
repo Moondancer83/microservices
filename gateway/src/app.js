1b83c993769303e579366120e7415af8cfac01da
Mark Kalinovits <mkalinovits@chemaxon.com>
2020-11-06 14:14:43 +0100

    Adds proxy to gateway service

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -5,7 +5,24 @@ const app = express();
 const port = 80;
 app.use(express.json());
 
+const proxyTo = async (url, req, res) => {
+  const serviceResponse = await axios.request({
+    url,
+    method: req.method,
+    data: req.body
+  });
+  res.json(serviceResponse.data);
+}
+
 app.route('/').get((req, res) => res.json({ service: 'gateway' }));
+
+app.route('/basket').get(async (req, res) => {
+  proxyTo("http://basket", req, res);
+})
+
+app.route('/catalog').get(async (req, res) => {
+  proxyTo("http://catalog", req, res);
+})
 
 app.listen(port, () => {
   console.log('Server started on port: ' + port);

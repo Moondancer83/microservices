4bdc4c5f16288b28a7978ba21793b44fb1851060
Mark Kalinovits <mkalinovits@chemaxon.com>
2020-11-06 15:42:39 +0100

    Adds full proxy

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -16,13 +16,15 @@ const proxyTo = async (url, req, res) => {
 
 app.route('/').get((req, res) => res.json({ service: 'gateway' }));
 
-app.route('/basket').get(async (req, res) => {
-  proxyTo("http://basket", req, res);
-})
+app.route('/basket').get((req, res) => proxyTo('http://basket', req, res).catch(console.error));
+app.route('/basket/set-item').patch((req, res) => proxyTo('http://basket/set-item', req, res).catch(console.error));
+app.route('/basket/show').get((req, res) => proxyTo('http://basket/show', req, res).catch(console.error));
+app.route('/basket/clear').delete((req, res) => proxyTo('http://basket/clear', req, res).catch(console.error));
 
-app.route('/catalog').get(async (req, res) => {
-  proxyTo("http://catalog", req, res);
-})
+app.route('/catalog').get((req, res) => proxyTo('http://catalog', req, res).catch(console.error));
+app.route('/catalog/product').get((req, res) => proxyTo('http://catalog/product', req, res).catch(console.error));
+app.route('/catalog/product/:id').get((req, res) => proxyTo(`http://catalog/product/${req.params.id}`, req, res).catch(console.error));
+app.route('/catalog/product/:id').patch((req, res) => proxyTo(`http://catalog/product/${req.params.id}`, req, res).catch(console.error));
 
 app.listen(port, () => {
   console.log('Server started on port: ' + port);
